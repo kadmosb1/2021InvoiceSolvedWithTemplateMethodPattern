df92fb670f6764eea6bc9bc93a7e2493a3af1cde
Karel J. van der Lelij <kadmosb1@users.noreply.github.com>
2021-05-12 14:00:46 +0200

    Code smell 'duplicate code' in getLastLinesOfInvoice opgeruimd.

--- src/main/java/Business.java
+++ src/main/java/Business.java
@@ -11,6 +11,18 @@ public class Business {
         this.type = type;
     }
 
+    private StringBuilder getDefaultLastLines (double priceExcludingVAT) {
+
+        // Op de eerste regel wordt óf 'subtotaal' getoond (bij normale ondernemers) of 'totaal' (want bij kleine
+        // ondernemers wordt geen BTW in rekening gebracht).
+        String subject = type == NORMAL ? "Subtotaal" : "Totaal";
+
+        StringBuilder lines = new StringBuilder ();
+        lines.append (InvoiceFormatter.getLine (true, "---------- +"));
+        lines.append (InvoiceFormatter.getLine (true, "%-9s € %8.2f  ", subject, priceExcludingVAT));
+        return lines;
+    }
+
     private double getPriceIncludingVAT(double priceExcludingVAT) {
         return (1 + VAT / 100.0) * priceExcludingVAT;
     }
@@ -22,23 +34,21 @@ public class Business {
     public String getLastLinesOfInvoice (double priceExcludingVAT) {
 
         StringBuilder lines = new StringBuilder ();
+
+        lines.append (getDefaultLastLines (priceExcludingVAT));
+
         if (type == NORMAL) {
             double vat = getVAT (priceExcludingVAT);
             double priceIncludingVAT = getPriceIncludingVAT (priceExcludingVAT);
-
-            lines.append (InvoiceFormatter.getLine (true, "---------- +"));
-            lines.append (InvoiceFormatter.getLine (true, "Subtotaal € %8.2f  ", priceExcludingVAT));
             lines.append (InvoiceFormatter.getLine (true, "21%% BTW   € %8.2f  ", vat));
             lines.append (InvoiceFormatter.getLine (true, "---------- +"));
             lines.append (InvoiceFormatter.getLine (true, "Totaal    € %8.2f  ", priceIncludingVAT));
-            return lines.toString ();
         }
         else { // type = NORMAL
-            lines.append (InvoiceFormatter.getLine (true, "----------+"));
-            lines.append (InvoiceFormatter.getLine (true, "Totaal    € %8.2f  ", priceExcludingVAT));
             lines.append (InvoiceFormatter.getEmptyLine ());
             lines.append (InvoiceFormatter.getLine (true, "Factuur is vrijgesteld van OB o.g.v. artikel 25 Wet OB  "));
-            return lines.toString();
         }
+
+        return lines.toString ();
     }
 }
--- src/test/java/BusinessTest.java
+++ src/test/java/BusinessTest.java
@@ -8,10 +8,10 @@ class BusinessTest {
     void getLastLinesOfInvoiceForSmallBusiness () {
 
         Business business = new Business (Business.SMALL);
-        String expectedValue = "=                                                          ----------+ =\r\n" +
-                               "=                                                Totaal    €   100,00  =\r\n" +
+        String expectedValue = "=                                                         ---------- + =\r\n" +
+                               "=                                               Totaal    €   100,00   =\r\n" +
                                "=                                                                      =\r\n" +
-                               "=              Factuur is vrijgesteld van OB o.g.v. artikel 25 Wet OB  =\r\n";
+                               "=             Factuur is vrijgesteld van OB o.g.v. artikel 25 Wet OB   =\r\n";
         String actualValue = business.getLastLinesOfInvoice (100);
         assertEquals (expectedValue, actualValue);
 
@@ -20,11 +20,11 @@ class BusinessTest {
     @Test
     void getLastLinesOfInvoiceForNormalBusiness () {
         Business business = new Business (Business.NORMAL);
-        String expectedValue = "=                                                          ----------+ =\r\n" +
-                               "=                                                Subtotaal €   100,00  =\r\n" +
-                               "=                                                21% BTW   €    21,00  =\r\n" +
-                               "=                                                          ----------+ =\r\n" +
-                               "=                                                Totaal    €   121,00  =\r\n";
+        String expectedValue = "=                                                         ---------- + =\r\n" +
+                               "=                                               Subtotaal €   100,00   =\r\n" +
+                               "=                                               21% BTW   €    21,00   =\r\n" +
+                               "=                                                         ---------- + =\r\n" +
+                               "=                                               Totaal    €   121,00   =\r\n";
         String actualValue = business.getLastLinesOfInvoice (100);
         assertEquals (expectedValue, actualValue);
     }
